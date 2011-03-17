1b8595b7aea41643594f9234be70c6bbdef07fd9
Ivan Koblik <ikoblik@hotmail.com>
2011-03-17 11:46:45 +0100

    Implemented the good suffix rule for Boyer-Moore algorithm

--- src/str_match/classic.clj
+++ src/str_match/classic.clj
@@ -25,10 +25,41 @@
        :else
        (let [zkp (nth zs (- k l)) betta (- r k -1)]
          (cond
-    ;;zk-prime is less than betta, then zk=zkp
-    (< zkp betta) (recur (inc k) (conj! zs zkp) l r (rest tail))
-    ;;zk-prime is greater than betta, then zk=betta and l=k
-    (> zkp betta) (recur (inc k) (conj! zs betta) k r (rest tail))
-    ;;zkp stopped exactly at the border of zl new character at the end of betta may match
-    :else (let [zk (+ betta (count-match (drop betta chain) (drop betta tail)))]
-      (recur (inc k) (conj! zs zk) k (+ k zk -1) (rest tail)))))))))
+	  ;;zk-prime is less than betta, then zk=zkp
+	  (< zkp betta) (recur (inc k) (conj! zs zkp) l r (rest tail))
+	  ;;zk-prime is greater than betta, then zk=betta and l=k
+	  (> zkp betta) (recur (inc k) (conj! zs betta) k r (rest tail))
+	  ;;zkp stopped exactly at the border of zl new character at the end of betta may match
+	  :else (let [zk (+ betta (count-match (drop betta chain) (drop betta tail)))]
+		  (recur (inc k) (conj! zs zk) k (+ k zk -1) (rest tail)))))))))
+
+
+(defn find-L
+  ([chain]
+     "Implements good-suffix rule for Boyer-Moore algorithm,
+      chain is a sequence of elements from a finite set"
+     (find-L chain (reverse (find-z (reverse chain)))))
+  ([chain n-values]
+     "Implements good-suffix rule for Boyer-Moore algorithm,
+      chain - is a sequence of elements from a finite set,
+      n-values - is a reverse of z-values of the reversed chain
+      returns - the L'(i) indices of substrings [1..L'(i)]
+                such that their suffixes are equal to [i..n]
+      see Gusfield p. 20"
+     (when (seq chain) ;discard empty sequences
+       (let [n (count chain)
+	     n-2 (- n 2)]
+	 (loop [l-val-map (transient {}) cc 0]
+	   (if (> cc n-2)
+	     (persistent! 
+	      (reduce (fn [v i] (conj! v (or (l-val-map i) 0)))
+		      (transient [])
+		      (take n (iterate inc 0))))
+	     (recur (conj! l-val-map [(- n (nth n-values cc)) cc]) (inc cc))))))))
+
+(defn find-l
+  [n-values]
+  (when n-values
+    (let [n (count (n-values))]
+      (map =
+       (map list (iterate inc 0) n-values)))))
--- test/str_match/classic_test.clj
+++ test/str_match/classic_test.clj
@@ -2,40 +2,40 @@
   (:use [str-match.classic] :reload-all)
   (:use [clojure.test]))
 
-(deftest empty-string-or-collection
+(deftest find-z-empty-string-or-collection
   (is (= (find-z "") nil))
   (is (= (find-z nil) nil))
   (is (= (find-z []) nil)))
 
-(deftest fails-with-invalid-argument
+(deftest find-z-fails-with-invalid-argument
   (is (thrown? IllegalArgumentException (find-z 'symbol)))
   (is (thrown? IllegalArgumentException (find-z :keyword))))
 
-(deftest single-character
+(deftest find-z-single-character
   (is (= (find-z "aaaaaa") [0 5 4 3 2 1]))
   (is (= (find-z "a") [0])))
 
-(deftest periodic-string
+(deftest find-z-periodic-string
   (is (= (find-z "abcabcabcabc") [0 0 0 9 0 0 6 0 0 3 0 0]))
   (is (= (find-z "abababab") [0 0 6 0 4 0 2 0])))
 
-(deftest zkp-eq-beta
+(deftest find-z-zkp-eq-beta
   (testing "beta string can be extended"
     (is (= (find-z "ab!a ab!aba") [0 0 0 1 0 4 0 0 2 0 1])))
   (testing "beta string cannot be extended"
     (is (= (find-z "ab!a ab!ada") [0 0 0 1 0 4 0 0 1 0 1]))))
   
-(deftest zkp-ne-beta
+(deftest find-z-zkp-ne-beta
   (testing "zk-prime < beta"
     (is (= (find-z "ab!ad ab!ada") [0 0 0 1 0 0 5 0 0 1 0 1])))
   (testing "zk-prime > beta"
     (is (= (find-z "ab!ab ab!ada") [0 0 0 2 0 0 4 0 0 1 0 1]))))
   
-(deftest non-periodic-string
+(deftest find-z-non-periodic-string
   (is (= (find-z "abcabdabcabd") [0 0 0 2 0 0 6 0 0 2 0 0]))
   (is (= (find-z "abc$abcabc") [0 0 0 0 3 0 0 3 0 0])))
   
-(deftest non-string
+(deftest find-z-non-string
   (testing "similar to string [ab!a ab!aba ab!aca]"
     (is (= (find-z [1 2 nil 1 'space 1 2 nil 1 2 1 'space 1 2 nil 1 3 1]) 
       [0 0 0 1 0 4 0 0 2 0 1 0 4 0 0 1 0 1])))
@@ -45,3 +45,8 @@
   (testing "similar to string [ab!ab ab!ada]"
     (is (= (find-z [1 2 nil 1 2 'space 1 2 nil 1 3 1]) 
       [0 0 0 2 0 0 4 0 0 1 0 1]))))
+
+(deftest find-L-empty-string-or-collection
+  (is (= (find-L "") nil))
+  (is (= (find-L nil) nil))
+  (is (= (find-L []) nil)))
