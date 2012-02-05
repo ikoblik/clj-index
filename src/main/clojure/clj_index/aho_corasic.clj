c20a1c34218933e60debe74637734691e238c9b2
Ivan Koblik <ikoblik@hotmail.com>
2012-02-05 07:50:15 +0100

    Implemented Aho-Corasic dictionary indexing method.

--- src/main/clojure/clj_index/aho_corasic.clj
+++ src/main/clojure/clj_index/aho_corasic.clj
@@ -1,10 +1,74 @@
 (in-ns 'clj-index.core)
 (require '[clojure.zip :as zip])
+(use '[mutable.box])
 
 ;;===============================================================
 ;; Aho-Corasic matching algorithm
 ;;===============================================================
 
-(defn add-word [tree word]
-  )
+;;TODO: Storage to children that autopromotes itself to
+;;hash map as it grows
 
+(defn- get-child
+  "Returns mutable child object for the given key, or null."
+  [node key]
+  (get-in @node [:children key]))
+
+(defn- get-children
+  "Returns key/value storage of all the node's children"
+  [node]
+  (get @node :children))
+
+(defn- make-child
+  "Creates mutable child object"
+  []
+  (box (array-map)))
+
+(defn- get-or-add!
+  "Searches for a child with the given key, if it does not
+   exist, creates a new child and updates node's state."
+  [node key]
+  (let [child (get-child node key)]
+    (if child
+      child
+      (let [child (make-child)]
+        (set-value! node
+                        (assoc-in @node [:children key] child))
+        child))))
+
+(defn- mark-word!
+  "Marks given node as an ending of a word"
+  [node]
+  (set-value! node (assoc @node :stop true)))
+
+(defn- word? [node]
+  (get @node :stop))
+
+;;TODO Use Flyweight pattern. Write a function that memoizes
+;;every item from (seq word)?
+(defn add-word! [node word]
+  (when (seq word)
+    (letfn [(recur-add [node word]
+              (if (seq word)
+                (recur (get-or-add! node (first word))
+                       (rest word))
+                node))]
+      (mark-word!
+       (recur-add node word)))
+    node))
+
+(defn get-skip-link [node]
+  (get @node :skip))
+
+(defn set-skip-link!
+  "Sets skip link for the given node. Link also specifies
+   linked node depth."
+  [node skip-node length]
+  (set-value! node
+              (assoc @node :skip [skip-node length])))
+
+(defn add-skip-links!
+  "Walks the tree in breadth first fashion and adds skip link
+   to its nodes."
+  [root]
+  )
--- src/test/clojure/clj_index/aho_corasic_test.clj
+++ src/test/clojure/clj_index/aho_corasic_test.clj
@@ -1,8 +1,48 @@
 (ns clj-index.aho-corasic-test
   (:use [clj-index.core :as sut] :reload-all)
-  (:use [clojure.test]))
+  (:use [clojure.test]
+        [mutable.box]))
 
 ;;===============================================================
 ;; Aho-Corasic unit tests
 ;;===============================================================
 
+(deftest get-or-add
+  (testing "Intialization"
+    (let [b (box)]
+      (is (= (@#'sut/make-child)
+             (@#'sut/get-or-add! b \a))
+          "New child")
+      (is (= (@#'sut/make-child)
+             (@#'sut/get-child b \a))
+          "Original box updated"))))
+
+(deftest get-children
+  (let [b (doto (box)
+            (@#'sut/get-or-add! \a)
+            (@#'sut/get-or-add! \b)
+            (@#'sut/get-or-add! \c))]
+    (is (= #{\a \b \c}
+           (into #{}
+                 (keys (@#'sut/get-children b))))
+        "All originally added keys")
+    (doseq [[k v] (@#'sut/get-children b)]
+      (is (= (@#'sut/make-child)
+             v)
+          (str "New child node for key " k)))))
+
+(deftest word-marks
+  (let [b (box)]
+    (is (not (@#'sut/word? b)))
+    (do (@#'sut/mark-word! b)
+        (is (@#'sut/word? b)))))
+
+(deftest skip-links
+  (let [b1 (box)
+        b2 (box)]
+    (are [b] (nil? (@#'sut/get-skip-link b))
+         b1
+         b2)
+    (do (@#'sut/set-skip-link! b1 b2 1)
+        (is (= [b2 1]
+               (@#'sut/get-skip-link b1))))))
