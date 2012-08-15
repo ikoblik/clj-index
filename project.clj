94abca2c3ec01cf20d83ec43e199971d08ee53ec
ikoblik <Ivan.Koblik@cern.ch>
2012-08-15 15:32:12 +0200

    Moving to Clojure v1.4.0, expiriencing issues with leiningen removing compiled class files.

--- project.clj
+++ project.clj
@@ -1,10 +1,13 @@
 (defproject clj-index "0.0.1-SNAPSHOT"
   :description "String indexing and matching algorithms"
-  :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]]
-  :dev-dependencies [[com.github.technomancy/swank-clojure "1.2.1"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  ;:plugins [[lein-swank "1.4.4"]]
+  :java-source-paths ["src/main/java"]
+  :java-source-path "src/main/java"  
+
   :source-path "src/main/clojure"
   :test-path "src/test/clojure"
 
-  :java-source-path "src/main/java"
-  :compile-path "classes")
+  :compile-path "classes"
+  :prep-tasks ["javac" "compile"]
+)
