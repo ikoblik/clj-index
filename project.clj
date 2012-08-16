319995e424e62a18ad4fdd482922dcdf7cf63125
Ivan Koblik <ikoblik@hotmail.com>
2012-08-16 08:20:56 +0200

    Updated project.clj to lein 2 format.

--- project.clj
+++ project.clj
@@ -1,13 +1,10 @@
 (defproject clj-index "0.0.1-SNAPSHOT"
   :description "String indexing and matching algorithms"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  ;:plugins [[lein-swank "1.4.4"]]
+  :plugins [[lein-swank "1.4.4"]]
   :java-source-paths ["src/main/java"]
-  :java-source-path "src/main/java"  
-
-  :source-path "src/main/clojure"
-  :test-path "src/test/clojure"
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
 
   :compile-path "classes"
-  :prep-tasks ["javac" "compile"]
 )
